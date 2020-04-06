93d6a42debff4f882662394c2d364bc25b7bd44e
senjuid <senjuid@gmail.com>
2020-04-06 07:16:55 +0700

    Update AutoStartManagerPlugin.java
    update

--- src/android/AutoStartManagerPlugin.java
+++ src/android/AutoStartManagerPlugin.java
@@ -1,20 +1,19 @@
 package com.dataon.cordova.plugin.autostartmanager;
 
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaInterface;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-
 import android.app.Activity;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.provider.Settings;
-import android.os.Build;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.hardware.Camera;
+import android.os.Build;
+import android.provider.Settings;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
 
 
 /**
@@ -22,102 +21,106 @@ import android.content.Intent;
  */
 public class AutoStartManagerPlugin extends CordovaPlugin {
 
-    PackageManager pm;
+  @Override
+  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    if (action.equals("showStartManager")) {
+      this.showStartManager();
+      return true;
+    } else if (action.equals("checkAutomaticDateTimeZone")) {
+      JSONArray json = new JSONArray();
 
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("showStartManager")) {
-            this.showStartManager();
-            return true;
-        } else if (action.equals("checkAutomaticDateTimeZone")) {
-            JSONArray json = new JSONArray();
+      try {
+        boolean result = this.isAutomaticChecked();
 
-            try {
-                boolean result = this.isAutomaticChecked();
+        Context context = this.cordova.getActivity().getApplicationContext();
+        final CordovaInterface cordova = this.cordova;
 
-                Context context = this.cordova.getActivity().getApplicationContext();
-                final CordovaInterface cordova = this.cordova;
+        if (!result) {
+          AlertDialog alertDialog = new AlertDialog.Builder(cordova.getActivity()).create();
+          alertDialog.setTitle("Enable Automatic Date Time");
+          alertDialog.setCancelable(false);
+          alertDialog.setMessage("Your Automatic Date Time or Time Zone Settings is set to 'Off'.\nPlease Enable to use this feature");
+          alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Show Settings",
+            new DialogInterface.OnClickListener() {
+              public void onClick(DialogInterface dialog, int which) {
+                Intent i = new Intent(android.provider.Settings.ACTION_DATE_SETTINGS);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(i);
+                dialog.dismiss();
 
-                if(!result) {
-                    AlertDialog alertDialog = new AlertDialog.Builder(cordova.getActivity()).create();
-                    alertDialog.setTitle("Enable Automatic Date Time");
-                    alertDialog.setCancelable(false);
-                    alertDialog.setMessage("Your Automatic Date Time or Time Zone Settings is set to 'Off'.\nPlease Enable to use this feature");
-                    alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Show Settings",
-                            new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int which) {
-                                    Intent i = new Intent(android.provider.Settings.ACTION_DATE_SETTINGS);
-                                    i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                                    context.startActivity(i);
-                                    dialog.dismiss();
-
-                                    try {
-                                        json.put(0, false);
-                                    } catch (JSONException e) {
-                                        e.printStackTrace();
-                                    }
-                                    callbackContext.success(json);
-                                }
-                            });
-                    alertDialog.show();
-                } else {
-                    json.put(0, true);
-                    callbackContext.success(json);
+                try {
+                  json.put(0, false);
+                } catch (JSONException e) {
+                  e.printStackTrace();
                 }
-            } catch (Settings.SettingNotFoundException e) {
-                json.put(0, "SettingNotFoundException");
                 callbackContext.success(json);
-            }
-
-            return true;
-        } else if (action.equals("checkCamera")) {
-          JSONArray json = new JSONArray();
-          Context context = this.cordova.getActivity().getApplicationContext();
-          pm = context.getPackageManager();
-          if (pm.hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)) {
-            json.put(0, true);
-          } else {
-            json.put(0, false);
-          }
+              }
+            });
+          alertDialog.show();
+        } else {
+          json.put(0, true);
           callbackContext.success(json);
         }
-        return false;
+      } catch (Settings.SettingNotFoundException e) {
+        json.put(0, "SettingNotFoundException");
+        callbackContext.success(json);
+      }
+
+      return true;
+    } else if (action.equals("checkCamera")) {
+      JSONArray json = new JSONArray();
+      json.put(0, isCameraUsebyApp());
+      callbackContext.success(json);
+    }
+    return false;
+  }
+
+  private boolean isCameraUsebyApp() {
+    Camera camera = null;
+    try {
+      camera = Camera.open();
+    } catch (RuntimeException e) {
+      return false;
+    } finally {
+      if (camera != null) camera.release();
+    }
+    return true;
+  }
+
+  private void showStartManager() {
+    Context context = this.cordova.getActivity().getApplicationContext();
+    new AutoStartPermissionHelper().getAutoStartPermission(context);
+  }
+
+  private boolean isAutomaticChecked() throws Settings.SettingNotFoundException {
+
+    Integer dateTime, timezone;
+
+    Activity activity = this.cordova.getActivity();
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+      dateTime = Settings.Global.getInt(activity.getContentResolver(), Settings.Global.AUTO_TIME, 0);
+      timezone = Settings.Global.getInt(activity.getContentResolver(), Settings.Global.AUTO_TIME_ZONE, 0);
+
+    } else {
+      dateTime = android.provider.Settings.System.getInt(
+        activity.getContentResolver(),
+        android.provider.Settings.System.AUTO_TIME_ZONE,
+        0
+      );
+
+      timezone = android.provider.Settings.System.getInt(
+        activity.getContentResolver(),
+        android.provider.Settings.System.AUTO_TIME,
+        0
+      );
     }
 
-    private void showStartManager() {
-        Context context = this.cordova.getActivity().getApplicationContext();
-        new AutoStartPermissionHelper().getAutoStartPermission(context);
+    if (dateTime == 0 || timezone == 0) {
+      return false;
+    } else {
+      return true;
     }
-
-    private boolean isAutomaticChecked() throws Settings.SettingNotFoundException {
-
-        Integer dateTime, timezone;
-
-        Activity activity = this.cordova.getActivity();
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-            dateTime = Settings.Global.getInt(activity.getContentResolver(), Settings.Global.AUTO_TIME, 0);
-            timezone = Settings.Global.getInt(activity.getContentResolver(), Settings.Global.AUTO_TIME_ZONE, 0);
-
-        } else {
-            dateTime = android.provider.Settings.System.getInt(
-                    activity.getContentResolver(),
-                    android.provider.Settings.System.AUTO_TIME_ZONE,
-                    0
-            );
-
-            timezone = android.provider.Settings.System.getInt(
-                    activity.getContentResolver(),
-                    android.provider.Settings.System.AUTO_TIME,
-                    0
-            );
-        }
-
-        if (dateTime == 0 || timezone == 0) {
-            return false;
-        } else {
-            return true;
-        }
-    }
+  }
 
 }
